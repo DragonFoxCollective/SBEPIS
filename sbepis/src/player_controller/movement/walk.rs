8d38d45dc0494ed43a0cb0ffc87fe3bde9d43055
Jady Wright <iamjwright159@gmail.com>
2025-08-18 18:18:49 -0400

    cargo fmt

--- sbepis/src/player_controller/movement/walk.rs
+++ sbepis/src/player_controller/movement/walk.rs
@@ -29,14 +29,14 @@ use super::stand::Standing;
 	air_acceleration: 2.0,
 })]
 pub struct PlayerWalkSettings {
-	pub speed: f32,
-	pub sneak_speed: f32,
-	pub sprint_speed: f32,
+    pub speed: f32,
+    pub sneak_speed: f32,
+    pub sprint_speed: f32,
 
-	pub friction: f32,
-	pub air_friction: f32,
-	pub acceleration: f32,
-	pub air_acceleration: f32,
+    pub friction: f32,
+    pub air_friction: f32,
+    pub acceleration: f32,
+    pub air_acceleration: f32,
 }
 
 #[derive(Component, Default)]
@@ -48,12 +48,12 @@ pub struct Walking;
 	run_if = button_just_pressed(PlayerAction::Move),
 )]
 fn standing_to_walking(
-	players: Query<Entity, (With<PlayerBody>, With<Standing>)>,
-	mut commands: Commands,
+    players: Query<Entity, (With<PlayerBody>, With<Standing>)>,
+    mut commands: Commands,
 ) {
-	for player in players.iter() {
-		commands.entity(player).remove::<Standing>().insert(Walking);
-	}
+    for player in players.iter() {
+        commands.entity(player).remove::<Standing>().insert(Walking);
+    }
 }
 
 #[add_system(
@@ -62,12 +62,12 @@ fn standing_to_walking(
 	run_if = button_just_released(PlayerAction::Move),
 )]
 fn walking_to_standing(
-	players: Query<Entity, (With<PlayerBody>, With<Walking>)>,
-	mut commands: Commands,
+    players: Query<Entity, (With<PlayerBody>, With<Walking>)>,
+    mut commands: Commands,
 ) {
-	for player in players.iter() {
-		commands.entity(player).remove::<Walking>().insert(Standing);
-	}
+    for player in players.iter() {
+        commands.entity(player).remove::<Walking>().insert(Standing);
+    }
 }
 
 #[add_system(
@@ -76,69 +76,69 @@ fn walking_to_standing(
 	before = ExecuteMovementSet,
 )]
 fn update_walk_velocity(
-	mut movement: Query<
-		(
-			&mut Movement,
-			&Velocity,
-			&Transform,
-			&DirectionalInput,
-			Has<Walking>,
-			Has<Sprinting>,
-			Has<Sneaking>,
-			Has<Grounded>,
-		),
-		Or<(
-			With<Standing>,
-			With<Walking>,
-			With<Sprinting>,
-			With<Crouching>,
-			With<Sneaking>,
-			With<ChargeStanding>,
-			With<ChargeCrouching>,
-			With<ChargeWalking>,
-		)>,
-	>,
-	walk_settings: Res<PlayerWalkSettings>,
-	time: Res<Time>,
+    mut movement: Query<
+        (
+            &mut Movement,
+            &Velocity,
+            &Transform,
+            &DirectionalInput,
+            Has<Walking>,
+            Has<Sprinting>,
+            Has<Sneaking>,
+            Has<Grounded>,
+        ),
+        Or<(
+            With<Standing>,
+            With<Walking>,
+            With<Sprinting>,
+            With<Crouching>,
+            With<Sneaking>,
+            With<ChargeStanding>,
+            With<ChargeCrouching>,
+            With<ChargeWalking>,
+        )>,
+    >,
+    walk_settings: Res<PlayerWalkSettings>,
+    time: Res<Time>,
 ) {
-	for (mut movement, velocity, transform, di, walking, sprinting, sneaking, grounded) in
-		movement.iter_mut()
-	{
-		// Set up vectors
-		let velocity = (transform.rotation.inverse() * velocity.linvel).xz();
-		let wish_velocity = di.input
-			* if walking {
-				walk_settings.speed
-			} else if sprinting {
-				walk_settings.sprint_speed
-			} else if sneaking {
-				walk_settings.sneak_speed
-			} else {
-				0.0
-			};
-		let wish_speed = wish_velocity.length();
-		let wish_direction = wish_velocity.normalize_or_zero();
-		let friction = if grounded {
-			walk_settings.friction
-		} else {
-			walk_settings.air_friction
-		};
-		let acceleration = if grounded {
-			walk_settings.acceleration
-		} else {
-			walk_settings.air_acceleration
-		};
+    for (mut movement, velocity, transform, di, walking, sprinting, sneaking, grounded) in
+        movement.iter_mut()
+    {
+        // Set up vectors
+        let velocity = (transform.rotation.inverse() * velocity.linvel).xz();
+        let wish_velocity = di.input
+            * if walking {
+                walk_settings.speed
+            } else if sprinting {
+                walk_settings.sprint_speed
+            } else if sneaking {
+                walk_settings.sneak_speed
+            } else {
+                0.0
+            };
+        let wish_speed = wish_velocity.length();
+        let wish_direction = wish_velocity.normalize_or_zero();
+        let friction = if grounded {
+            walk_settings.friction
+        } else {
+            walk_settings.air_friction
+        };
+        let acceleration = if grounded {
+            walk_settings.acceleration
+        } else {
+            walk_settings.air_acceleration
+        };
 
-		// Apply friction
-		let friction = -time.delta_secs() * friction * velocity;
-		let velocity = velocity + friction;
+        // Apply friction
+        let friction = -time.delta_secs() * friction * velocity;
+        let velocity = velocity + friction;
 
-		// Do funny quake movement
-		let funny_quake_speed = velocity.dot(wish_direction);
-		let add_speed = (wish_speed - funny_quake_speed)
-			.clamp(0.0, acceleration * wish_speed * time.delta_secs()); // TODO: In absolute units, ignores relativity
-		let new_velocity = velocity + wish_direction * add_speed;
+        // Do funny quake movement
+        let funny_quake_speed = velocity.dot(wish_direction);
+        let add_speed = (wish_speed - funny_quake_speed)
+            .clamp(0.0, acceleration * wish_speed * time.delta_secs()); // TODO: In absolute units, ignores relativity
+        let new_velocity = velocity + wish_direction * add_speed;
 
-		movement.0 = transform.rotation * Vec3::new(new_velocity.x, 0.0, new_velocity.y);
-	}
+        movement.0 = transform.rotation * Vec3::new(new_velocity.x, 0.0, new_velocity.y);
+    }
 }
